7852b44a1892d88a66efc431d9ea12a28d066544
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-08-24 16:53:45 -0500

    Install CleanWebpackPlugin. Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,31 @@
 const path = require('path');
 const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './js/index.js'
+  entry: './js/index.js',
+
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin({
+      // Simulate the removal of files: default: false
+      dry: true,
+      // Write Logs to Console, default: false
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild: default: true
+      cleanStaleWebpackAssets: true,
+      // Do not allow removal of current webpack assets: default: true
+      protectWebpackAssets: false
+    })
+  ]
+
 };
