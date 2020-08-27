9a5efce5dd2668d6442bf03547cc4f08aa5eb226
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-08-27 18:53:33 -0500

    Update sass. Add _typography.scss. Rename _bdg.scss to _layout.scss

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import '../_sass/bootstrap/bootstrap.scss';
 import '../_sass/_colors.scss';
+import '../_sass/_typography.scss';
+import '../_sass/_layout.scss';
 import '../_sass/_navbar-bdg.scss';
-import '../_sass/_bdg.scss';
